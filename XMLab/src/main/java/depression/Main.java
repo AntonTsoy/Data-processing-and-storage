95c3a7a2d3b049ddc347523134954e6f4c106662
Anton Tsoy <tcoy210@gmail.com>
2025-05-23 09:20:13 +0700

    Added metrics of execution

--- XMLab/src/main/java/depression/Main.java
+++ XMLab/src/main/java/depression/Main.java
@@ -3,6 +3,8 @@ package depression;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
+
         XMLParser parser = new XMLParser("people.xml");
         FragmentMerger resolver = new FragmentMerger();
         PersonFragment person = parser.parsePersonFragment();
@@ -13,5 +15,9 @@ public class Main {
         parser.close();
         resolver.structurePersonFragments();
         XMLUtils.marshalPeople(resolver.getPersons(), "schema.xsd", "result.xml");
+
+        long endTime = System.nanoTime();
+        long executionTime = (endTime - startTime) / 1_000_000;
+        System.out.println("Execution time: " + executionTime + " ms");
     }
 }
